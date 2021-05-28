88ce5bb071cf64959d5f5ccd6036b14f1279b99b
G Chao <81912588+powerlesbian@users.noreply.github.com>
2021-05-28 14:52:51 +0800

    colors, mascot, founded, motto updated

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    colors: ['Black', 'Silver Gray','Cardinal'],
+    mascot: "Beaver",
+    founded: 1861,
+    motto: 'Mens et Manus',
 };
